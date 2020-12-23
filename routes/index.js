78a355175aef1bbb73e5767ed464cd402fb71cff
Vinit <vsharma7dec@gmail.com>
2020-12-23 16:28:51 +0530

    add a controller and a router for home

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+// console.log('router loaded');
+
+const homeController = require('../controllers/home_controller');
+router.get('/', homeController.home);
+
+module.exports = router;
