4177e0ca0b868bf69d23b63c1c9e7b746d5f6c8b
Vinit <vsharma7dec@gmail.com>
2020-12-23 17:00:49 +0530

    added ejs as view engine

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
 router.get('/', homeController.home);
-
-module.exports = router;
+router.use('/users', require('./users'));
+module.exports = router; 
+ 
