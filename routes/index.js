662c45e672a3827ffa90d207fe887fb61faa96cd
Vinit <vsharma7dec@gmail.com>
2021-01-08 16:24:55 +0530

    added home, user controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-// console.log('router loaded');
+
+console.log('router loaded!');   
 
 const homeController = require('../controllers/home_controller');
 router.get('/', homeController.home);
-router.use('/users', require('./users'));
+router.use('/users', require('./users')); 
 module.exports = router; 
- 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const usersControllers = require('../controllers/users_controllers');
+const usersControllers = require('../controllers/users_controller');
+// console.log('user router loaded!');
 router.get('/profile', usersControllers.profile);
 
 module.exports = router;
